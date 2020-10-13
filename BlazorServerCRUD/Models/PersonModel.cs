7a22ab560e18ca97ca14e6e75aee9394ec3abae8
isakulaksiz <isakulaksiz@outlook.com>
2020-10-13 23:54:45 +0300

    Add razor component

---
+++ BlazorServerCRUD/Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerCRUD.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+    }
+}
